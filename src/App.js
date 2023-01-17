35e766f415af0723abb122ad40dd0e367bae3357
Julia <julia.maliutsina@gmail.com>
2023-01-17 12:40:27 +0300

    add styles & fonts

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header"></header>
+      <div className="App-content"></div>
+      <footer className="App-footer"></footer>
     </div>
   );
 }
